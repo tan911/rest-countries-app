44707077b4d6e3a311f24c26b4ee677342c46488
Jovan Lanutan <jolanutan@gmail.com>
2023-01-20 16:54:48 +0800

    new route for detailed country

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Navigation from "./routes/Navigation/Navigation";
 import Home from "./routes/Home/Home";
+import CountryDetailed from "./components/CountryDetailed";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="country" element={<CountryDetailed />}/>
       </Route>
     </Routes>
   );
