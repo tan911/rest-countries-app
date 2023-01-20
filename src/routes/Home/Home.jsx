c47a873c70a24f72ec0b321d9647df6517e21407
Jovan Lanutan <jolanutan@gmail.com>
2023-01-20 16:59:25 +0800

    passing data to show country to filtered data

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -6,21 +6,30 @@ import ShowCountry from "../../components/ShowCountry";
 
 function Home() {
   const [countries, setCountries] = useState([]);
+  const [filterData, setFilterData] = useState("");
 
   const fetchCountries = async () => {
-    const response = await axios.get("https://restcountries.com/v3.1/all");
-    setCountries(response.data);
+    try {
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(response.data);
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   useEffect(() => {
     fetchCountries();
   }, []);
 
+  const dataHandler = (data) => {
+    setFilterData(data);
+  };
+
   return (
     <main>
-      <Operation />
-      <div className="mt-10">
-        <ShowCountry countriesData={countries} />
+      <Operation onDataItem={dataHandler} />
+      <div className="my-10 2xl:my-16">
+        <ShowCountry countriesData={countries} fdata={filterData} />
       </div>
     </main>
   );
