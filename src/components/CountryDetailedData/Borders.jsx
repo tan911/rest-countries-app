278ea4e389f6c33e68be1b8bd2587d57eeb364ce
Jovan Lanutan <jolanutan@gmail.com>
2023-01-24 00:14:49 +0800

    filtering data with border name

--- src/components/CountryDetailedData/Borders.jsx
+++ src/components/CountryDetailedData/Borders.jsx
@@ -1,11 +1,21 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import CountriesContext from "../../context/countries";
+
 function Borders({ borders }) {
+  const { border } = useContext(CountriesContext);
+
+  const borderHandler = (element) => {
+    border(element);
+  };
+
   return (
     <div className="grid grid-cols-3 gap-5">
       {borders &&
         borders.map((element, index) => (
           <Link
+            onClick={() => borderHandler(element)}
             key={index}
             className="inline-block py-1 px-6 rounded-md shadow-md bg-white dark:bg-el-dark-blue"
           >
