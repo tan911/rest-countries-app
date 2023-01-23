269725b4862f9aafb12247719686c65ee77056d9
Jovan Lanutan <jolanutan@gmail.com>
2023-01-23 20:04:07 +0800

    link countrydetailed data components

--- src/routes/CountryDetailed/CountryDetailed.jsx
+++ src/routes/CountryDetailed/CountryDetailed.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as ArrowBack } from "../../assets/arrow-back-outline.svg";
+import CountryDetailedData from "../../components/CountryDetailedData/CountryDetailedData";
 import CountriesContext from "../../context/countries";
 
 function CountryDetailed() {
@@ -11,15 +12,10 @@ function CountryDetailed() {
     return country.name.common === countryName;
   });
 
-  console.log(filterCountryByName);
-
   return (
     <div className="p-7 px-6 md:px-10 2xl:pt-14 2xl:px-24">
       <div className="inline-block">
-        <Link
-          to="/"
-          className="py-2 px-4 shadow rounded-md bg-white dark:bg-el-dark-blue flex justify-center items-center gap-2"
-        >
+        <Link to="/" className="link-back">
           <ArrowBack aria-hidden="true" focusable="false" />
           Back
         </Link>
@@ -37,52 +33,7 @@ function CountryDetailed() {
                 className="country-detailed-image"
               />
             </div>
-            <div className="w-full flex flex-col gap-4 lg:my-auto lg:gap-6 2xl:gap-16">
-              <h2 className="text-2xl font-bold 2xl:text-5xl">
-                {country.name.common}
-              </h2>
-              <div className="flex flex-col gap-4 lg:flex-row lg:justify-between">
-                <div className="container-detailed">
-                  <p>
-                    Native Name: <span>{country.name.common}</span>
-                  </p>
-                  <p>
-                    Population:{" "}
-                    <span>
-                      {new Intl.NumberFormat("en-us").format(
-                        country.population
-                      )}
-                    </span>
-                  </p>
-                  <p>
-                    Region: <span>{country.region}</span>
-                  </p>
-                  <p>
-                    Sub Region: <span>{country.subregion}</span>
-                  </p>
-                  <p>
-                    Capital: <span>capital</span>
-                  </p>
-                </div>
-                <div className="container-detailed">
-                  <p>
-                    Top Level Domain: <span>{country.tld[0]}</span>
-                  </p>
-                  <p>
-                    Currencies: <span>currencies</span>
-                  </p>
-                  <p>
-                    Languanges: <span>languages</span>
-                  </p>
-                </div>
-              </div>
-              <div>
-                <h3 className="2xl:text-2xl">Border Countries: </h3>
-                <Link></Link>
-                <Link></Link>
-                <Link></Link>
-              </div>
-            </div>
+            <CountryDetailedData country={country} />
           </div>
         ))}
       </div>
