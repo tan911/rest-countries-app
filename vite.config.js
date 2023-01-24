6c8975534127f2502c80faad6dbba5deb15484fd
Jovan Lanutan <jolanutan@gmail.com>
2023-01-24 08:07:45 +0800

    fixing file path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Countries-API-with-color-theme-switcher/',
   plugins: [svgr(), react()],
 })
