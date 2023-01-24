8a159c0632adf41860c004b49bd12d2f2dc64d64
Jovan Lanutan <jolanutan@gmail.com>
2023-01-24 17:30:39 +0800

    deployed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rest-countries-app/',
   plugins: [svgr(), react()],
+  base: '/rest-countries-app/',
 })
