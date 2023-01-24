6d2655731dfcd2a00286b9030dc34a12cec085dd
Jovan Lanutan <jolanutan@gmail.com>
2023-01-24 21:43:09 +0800

    changing file path

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import { Provider } from "./context/countries";
+
+import "./index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
